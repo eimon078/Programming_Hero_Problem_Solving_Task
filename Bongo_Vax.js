e6b4e25e54e696784fd80a03b989fd3e1ac9a6fd
taief078 <eimonhossain078@gmail.com>
2021-12-31 15:56:09 +0600

    Task done without added testcase

--- Bongo_Vax.js
+++ Bongo_Vax.js
@@ -1,14 +1,3 @@
-const userInfo = [
-    { name: "sunil", age: 21, temperature: 98 },
-    { name: "Biplap", age: 22, temperature: 98 },
-    { name: "Kabir", age: 36, temperature: 99 },
-    { name: "Rahul", age: 37, temperature: 99 },
-    { name: "Kat", age: 41, temperature: 98 },
-    { name: "Paul", age: 42, temperature: 98 },
-    { name: "Nayem", age: 50, temperature: 100 },
-    { name: "Sabnaj", age: 51, temperature: 101 },
-]
-
 function vaxTrail(userData) {
     const output = userData.reduce((acc, curr) => {
         if (curr.temperature >= 100) {
@@ -30,9 +19,7 @@ function vaxTrail(userData) {
     output.B = output.B.sort((a, b) => a.age % 2 - b.age % 2 || a.age - b.age)
     output.C = output.C.sort((a, b) => a.age % 2 - b.age % 2 || a.age - b.age)
     output.D = output.D.sort((a, b) => a.age % 2 - b.age % 2 || a.age - b.age)
-    console.log(output.A);
-
     return output;
 }
 
-console.log(vaxTrail(userInfo));
+// console.log(vaxTrail(userInfo));
