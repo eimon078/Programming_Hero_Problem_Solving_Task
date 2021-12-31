20affe4b206bbd33eaccafb7ad737868fb3f2b2f
taief078 <eimonhossain078@gmail.com>
2021-12-31 15:54:17 +0600

    Task done

--- Bongo_Vax.js
+++ Bongo_Vax.js
@@ -2,9 +2,9 @@ const userInfo = [
     { name: "sunil", age: 21, temperature: 98 },
     { name: "Biplap", age: 22, temperature: 98 },
     { name: "Kabir", age: 36, temperature: 99 },
-    { name: "Rahul", age: 36, temperature: 99 },
-    { name: "Paul", age: 42, temperature: 98 },
+    { name: "Rahul", age: 37, temperature: 99 },
     { name: "Kat", age: 41, temperature: 98 },
+    { name: "Paul", age: 42, temperature: 98 },
     { name: "Nayem", age: 50, temperature: 100 },
     { name: "Sabnaj", age: 51, temperature: 101 },
 ]
@@ -25,6 +25,12 @@ function vaxTrail(userData) {
         }
         return acc;
     }, { A: [], B: [], C: [], D: [] })
+
+    output.A = output.A.sort((a, b) => a.age % 2 - b.age % 2 || a.age - b.age)
+    output.B = output.B.sort((a, b) => a.age % 2 - b.age % 2 || a.age - b.age)
+    output.C = output.C.sort((a, b) => a.age % 2 - b.age % 2 || a.age - b.age)
+    output.D = output.D.sort((a, b) => a.age % 2 - b.age % 2 || a.age - b.age)
+    console.log(output.A);
 
     return output;
 }
